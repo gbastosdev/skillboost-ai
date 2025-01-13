b03f455a2abeadcac979a86ce87f890662199622
Gabriel Bastos <gabrielbastosdev@gmail.com>
2025-01-13 20:12:22 -0300

    Added AI call functionality to controller

---
+++ ai-service-ts/controllers/ai_controller.ts
@@ -0,0 +1,22 @@
+import { HfInference } from "@huggingface/inference";
+import dotenv from 'dotenv'
+dotenv.config({path: 'C:/Users/GabrielBastos/Desktop/git/skillboost-ai/.env'})
+
+const client = new HfInference();
+
+async function chat(){
+  return await client.chatCompletion({
+    model: "Qwen/QwQ-32B-Preview",
+    messages: [
+      {
+        role: "user",
+        content: "What is the capital of France?"
+      }
+    ],
+    max_tokens: 500
+  });
+} 
+
+chat().then((result)=>{
+    console.log(result.choices[0].message)
+})
